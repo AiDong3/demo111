3e8e5317b515fface97da65c5b08e647d4c9114a
AiDong3 <67336424+AiDong3@users.noreply.github.com>
2021-12-03 12:48:32 -0800

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -18,14 +18,11 @@ function showinfo(data){
   var span3 = document.getElementById("span3");
   var button = document.getElementById("button")
 
-  span1.innerText = `My first degree was a ${data.data[0].degrees.bachelors.data.type} from ${data.data[0].degrees.bachelors.data.institution}
-  in ${data.data[0].degrees.bachelors.data.date}`;
+  span1.innerText = `My first degree was a ${data.data[0].degrees.bachelors.data.type} from ${data.data[0].degrees.bachelors.data.institution} in ${data.data[0].degrees.bachelors.data.date}`;
   
-  span2.innerText = `My second degree was a ${data.data[1].degrees.Master.data.type} from ${data.data[1].degrees.Master.data.institution}
-  in ${data.data[1].degrees.Master.data.date}`;
+  span2.innerText = `My second degree was a ${data.data[1].degrees.Master.data.type} from ${data.data[1].degrees.Master.data.institution} in ${data.data[1].degrees.Master.data.date}`;
 
-  span3.innerText = `My third degree was a ${data.data[2].degrees.Master.data.type} from ${data.data[2].degrees.Master.data.institution}
-  in ${data.data[2].degrees.Master.data.date}`;
+  span3.innerText = `My third degree was a ${data.data[2].degrees.Master.data.type} from ${data.data[2].degrees.Master.data.institution} in ${data.data[2].degrees.Master.data.date}`;
 
   containder.removeChild(button);
   containder.appendChild(span1);
