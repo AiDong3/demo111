aad3dfec26688d69660aa53d1aeaecc39db5d7cf
AiDong3 <67336424+AiDong3@users.noreply.github.com>
2021-12-02 16:47:03 -0800

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -4,18 +4,29 @@ document.getElementById("button").onclick = function (){
     await fetch(url)
       //get your data here, and check for the response status. If it's not 200, throw an error
       .then((response) => response.json())
-      .then((data) =>
-        document.write(
-          `My first degree was a ${data.data[0].degrees.bachelors.data.type} from ${data.data[0].degrees.bachelors.data.institution}
-           in ${data.data[0].degrees.bachelors.data.date}\
-           My second degree was a ${data.data[1].degrees.Master.data.type} from ${data.data[1].degrees.Master.data.institution}
-           in ${data.data[1].degrees.Master.data.date} \
-           My third degree was a ${data.data[2].degrees.Master.data.type} from ${data.data[2].degrees.Master.data.institution}
-           in ${data.data[2].degrees.Master.data.date}`
-
-        )
+      .then((data) => showinfo(data)
       );
   }
   
   getDegrees("./src/degree.json");
+}
+
+function showinfo(data){
+  var containder = document.getElementById("container");
+  var span1 = document.createElement('span');
+  var span2 = document.createElement('span');
+  var span3 = document.createElement('span');
+
+  span1.innerText = `My first degree was a ${data.data[0].degrees.bachelors.data.type} from ${data.data[0].degrees.bachelors.data.institution}
+  in ${data.data[0].degrees.bachelors.data.date}`;
+  
+  span2.innerText = `My second degree was a ${data.data[1].degrees.Master.data.type} from ${data.data[1].degrees.Master.data.institution}
+  in ${data.data[1].degrees.Master.data.date}`;
+
+  span3.innerText = `My third degree was a ${data.data[2].degrees.Master.data.type} from ${data.data[2].degrees.Master.data.institution}
+  in ${data.data[2].degrees.Master.data.date}`;
+
+  containder.appendChild(span1);
+  containder.appendChild(span2);
+  containder.appendChild(span3);
 }
